22d971e249486373842c0b2be3bfd19793ec4c4e
Sam Lalezari <sam.lalezari@gmail.com>
2024-01-14 16:41:46 -0500

    Stub out info screen

--- src/features/info/Info.tsx
+++ src/features/info/Info.tsx
@@ -1,7 +1,43 @@
 import React from "react"
 
 const Info = () => {
-  return <div>info</div>
+  return (
+    <div>
+      <div>
+        <h2>025 Pikachu</h2>
+        <p>Mouse Pokemon</p>
+      </div>
+      <div>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" />
+      </div>
+      <div>
+        <ul>
+          <li>ELECTR</li>
+        </ul>
+      </div>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td>Height</td>
+              <td>0.4 m</td>
+            </tr>
+            <tr>
+              <td>Weight</td>
+              <td>6.0 kg</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <p>
+          It stores electricity in the electric sacs on its cheeks. When it
+          releases pent-up energy in a burst, the electric power is equal to a
+          lightning bolt.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Info
